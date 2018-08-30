c38daaf79779065f250a41ca9d8be6c7bc8accf5
Michael Gladstein <mtg@Michaels-MacBook-Pro-2.local>
2018-08-30 15:06:27 -0400

    added more routes

--- routes.js
+++ routes.js
@@ -9,12 +9,14 @@ var request = require("request");
 
 router.use(bodyParser());
 
+//check connection
 router.get("/", function(req, res, next){
     res.json({
         "message": "You are connected!"
     });
 });
 
+//list all documents in dB
 router.get("/listZipCodeData", function(req, res, next){
     ZipCodeData.find({})
             .sort({zipCode: 1})
@@ -24,6 +26,24 @@ router.get("/listZipCodeData", function(req, res, next){
             });
 });
 
+//search data for zipcode
+router.get("/search/:zipcode", function(req, res, next){
+    ZipCodeData.findOne({zipCode: req.params.zipcode})
+        .exec(function(err, data){
+            if(err) return next(err);
+            res.json(data);
+        });
+});
+
+router.get("/over/:leads", function(req, res, next){
+    ZipCodeData.where('leads').gt(req.params.leads).exec(function(err, data){
+        if(err) return next(err);
+        res.json(data);
+    });
+});
+
+
+//post new data for a zipcode
 router.post("/newZipCodeData", function(req, res, next){
     var inData = new ZipCodeData(req.body);
     inData.save(function(err, inData){
